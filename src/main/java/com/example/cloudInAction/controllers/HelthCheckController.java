110b9b87007e94eb120f2d1efe4f36328537feef
Vadym_Shapoval <vadym_shapoval@epam.com>
2021-02-02 09:52:28 +0200

    initial commit

---
+++ src/main/java/com/example/cloudInAction/controllers/HelthCheckController.java
@@ -0,0 +1,16 @@
+package com.example.cloudInAction.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+@RestController
+public class HelthCheckController
+{
+	@GetMapping("/healthCheck")
+	public String healthCheck(){
+		return "Hello, current time is: " + new Date().toString();
+	}
+}
